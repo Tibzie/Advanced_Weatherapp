ad8f6c642e6150f29da9395a3ce023c44bb1a320
Tibor Gazsovics <gt008@freemail.hu>
2018-11-13 23:52:27 +0000

    Added class and minor changes

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -7,7 +7,7 @@ import { Jumbotron,
   Button } from 'reactstrap';
 
 const SearchForm = () => (
-      <div>
+      <div className="search-form">
         <Jumbotron>
           <h1 className="display-3">Welcome to our app</h1>
           <p className="lead">Please enter your desired city and country in the fields below.<br />
@@ -17,12 +17,12 @@ const SearchForm = () => (
           <Form>
             <FormGroup>
               <Label for="cityField">City: </Label>
-              <Input type="text" name="city" id="cityField" placeholder="city here... e.g.'Budapest'" />
+              <Input type="text" name="city" id="cityField" placeholder="city here... e.g. 'Budapest'" />
             </FormGroup>
 
             <FormGroup>
               <Label for="countryField">Country: </Label>
-              <Input type="text" name="country" id="countryField" placeholder="country here... e.g.'Hungary'" />
+              <Input type="text" name="country" id="countryField" placeholder="country here... e.g. 'Hungary'" />
             </FormGroup>
           </Form>
           <Button color="info" className="float-right">Search</Button>
