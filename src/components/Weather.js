892c21922220eaa3f17f77c1865501dd5dee5fcf
Tibor Gazsovics <gt008@freemail.hu>
2018-11-15 23:15:58 +0000

    added reactstrap component/extra info/font awesome icons

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,20 +1,35 @@
 import React from 'react';
-import { Row, Col } from 'reactstrap';
-
+import { Row, Col, UncontrolledAlert } from 'reactstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Weather = props => (
   <div>
     {
-      props.city && props.temperature && props.descicon && props.description &&
+      props.city && props.temperature && props.descicon && props.condition &&
       <Row className="main-tab">
         <Col sm="4" className="main-info">{props.city}</Col>
-        <Col sm="4" className="main-info">{props.temperature}</Col>
-        <Col sm="4" className="main-info"><img src={`http://openweathermap.org/img/w/${props.descicon}.png`} alt="weather icon"/> {props.description}</Col>
-        <hr/>
+        <Col sm="4" className="main-info">{props.temperature}&deg;C</Col>
+        <Col sm="4" className="main-info">
+        <img src={`http://openweathermap.org/img/w/${props.descicon}.png`} alt="weather icon"/> {props.condition}</Col>
       </Row>
     }
     {
-      props.error && <div>{props.error}</div>
+      props.error &&
+      <UncontrolledAlert color="danger" fade={false} className="error-message">
+      {props.error}
+      </UncontrolledAlert>
+
+    }
+    {
+      props.humidity && props.temp_min && props.temp_max && props.description &&
+      <Row className="sub-tab">
+        <Col sm="4" className="sub-info"><strong>Humidity:</strong> {props.humidity}</Col>
+        <Col sm="4" className="sub-info">
+          <FontAwesomeIcon icon="temperature-low" size="lg"/> {props.temp_min}&deg;C
+          <FontAwesomeIcon className="temp-high" icon="temperature-high" size="lg"/> {props.temp_max}&deg;C
+        </Col>
+        <Col sm="4" className="sub-info"><strong>Description:</strong> {props.description}</Col>
+      </Row>
     }
   </div>
 );
